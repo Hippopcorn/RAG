be73d51b07b4743afccdf49af4c5887ebd93fab7
Elsa Varache <evarache@z2r8p7.42lyon.fr>
2026-05-21 11:06:04 +0200

    ending chunk md file

--- src/Chunk.py
+++ src/Chunk.py
@@ -13,30 +13,31 @@ class Chunk(BaseModel):
 class Indexer(BaseModel):
     """ Handles retrieving all the interest files from the vllm directotory
         and index them into Chunks """
-    dir_path: Path = Path("vllm-0.10.1")
-    list_chunks: list[Chunk] = []
+    dir_path: Path
+    chunks_list: list[Chunk] = []
+    py_files_paths: list[str] = []
+    md_files_paths: list[str] = []
 
     def get_interest_paths(self):
         """ Get a filtered list with the paths of the .py and .md files
             located in the dir_path """
-        filtered_paths: list[Path] = []
 
-        for file_path in self.dir_path.rglob("*.py"):
+        for py_file_path in self.dir_path.rglob("*.py"):
 
-            if "tests" in file_path.parts or "__pycache__" in file_path.parts:
+            if ("tests" in py_file_path.parts
+                    or "__pycache__" in py_file_path.parts):
                 continue
 
-            filtered_paths.append(file_path)
+            self.py_files_paths.append(str(py_file_path))
 
-        for file_path in self.dir_path.rglob("*.md"):
+        for md_file_path in self.dir_path.rglob("*.md"):
 
-            if ("tests" in file_path.parts or "__pycache__" in file_path.parts
-                    or "pytest_cache" in file_path.parts):
+            if ("tests" in md_file_path.parts or "__pycache__"
+                    in md_file_path.parts or "pytest_cache"
+                    in md_file_path.parts):
                 continue
 
-            filtered_paths.append(file_path)
-
-        return filtered_paths
+            self.md_files_paths.append(str(md_file_path))
 
     def index_md_file(self, path: str):
         """ Read a md file, then split it at each \n\n, check alls splits and
@@ -44,13 +45,12 @@ class Indexer(BaseModel):
             split_oversized_block function to recut splitted_blocs bigger than
             2000 characters. Then, create blocks with titles and text, until
             2000 characters """
-        chunks: list[Chunk] = []
         actual_block: str = ""
         blocks_list: list[str] = []
         fusion_title_list: list[str] = []
 
         try:
-            with open(path, "r") as file:
+            with open(path, "r", encoding="utf-8") as file:
                 content = file.read()
                 splited_blocs = content.split("\n\n")
 
@@ -96,9 +96,14 @@ class Indexer(BaseModel):
                 if actual_block:
                     blocks_list.append(actual_block)
 
-                for i, bloc in enumerate(blocks_list):
-                    print(f"len bloc {i + 1}: {len(bloc)}")
-                    print(bloc)
+                search_start_position = 0
+                for bloc in blocks_list:
+                    search_start_position = self.create_chunk(
+                        bloc, content, path, search_start_position)
+
+                # -------DEBUG-------------
+                for chunk in self.chunks_list:
+                    print(chunk)
                     print("\n\n\n")
 
         except Exception as e:
@@ -106,14 +111,64 @@ class Indexer(BaseModel):
 
     def split_oversized_block(self, blocs_list: list[str]):
         """ Split all blocs whose length is greater than 2000.
-            Cut them at each \n and return a new list """
+            Cut them at each \n and return a new list.
+            Re-accumulate their lines safely under 2000 characters """
         new_list: list[str] = []
 
         for bloc in blocs_list:
             if len(bloc) > 2000:
-                sub_bloc_list = bloc.split("\n")
-                new_list.extend(sub_bloc_list)
+                lines = bloc.split("\n")
+                current_sub_bloc = ""
+
+                for line in lines:
+                    if len(line) > 2000:
+                        if current_sub_bloc:
+                            new_list.append(current_sub_bloc.strip())
+                            current_sub_bloc = ""
+                        for i in range(0, len(line), 2000):
+                            new_list.append(line[i:i+2000])
+                        continue
+
+                    if (current_sub_bloc and len(current_sub_bloc)
+                            + len(line) + 1 > 2000):
+                        new_list.append(current_sub_bloc.strip())
+                        current_sub_bloc = line
+                    else:
+                        if not current_sub_bloc:
+                            current_sub_bloc = line
+                        else:
+                            current_sub_bloc += "\n" + line
+
+                if current_sub_bloc:
+                    new_list.append(current_sub_bloc.strip())
             else:
                 new_list.append(bloc)
 
         return new_list
+
+    def create_chunk(self, text: str, content_file: str,
+                     file_path: str, search_start_position: int):
+        """ Create a chunk with a bloc of text, and calcul it first and last
+            index. Add it in the chunks_list """
+        first_idx = content_file.find(text, search_start_position)
+
+        if first_idx == -1:
+            first_idx = content_file.find(text)
+
+        last_idx = first_idx + len(text)
+
+        chunk = Chunk(
+                        id=len(self.chunks_list),
+                        text=text,
+                        file_path=file_path,
+                        first_index=first_idx,
+                        last_index=last_idx
+                        )
+        self.chunks_list.append(chunk)
+
+        return last_idx
+
+    def process_files(self):
+        for file_path in self.md_files_paths:
+            print("coucou")
+            self.index_md_file(file_path)
--- src/__main__.py
+++ src/__main__.py
@@ -1,13 +1,14 @@
 from .Chunk import Indexer
+from pathlib import Path
 
 
 def main():
     try:
-        indexer = Indexer()
-        interest_files_path: list = indexer.get_interest_paths()
-        # print(interest_files_path)
+        indexer = Indexer(dir_path=Path("vllm-0.10.1"))
+        indexer.get_interest_paths()
+        indexer.process_files()
 
-        indexer.index_md_file("vllm-0.10.1/README.md")
+        # indexer.index_md_file("vllm-0.10.1/RELEASE.md")
     except Exception as e:
         print(e)
 
