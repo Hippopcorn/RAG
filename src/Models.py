913050f9fe0406134613758d74f705026da84ba5
Elsa Varache <evarache@z2r9p3.42lyon.fr>
2026-06-05 15:53:26 +0200

    add docstrings

--- src/Models.py
+++ src/Models.py
@@ -4,32 +4,34 @@ import uuid
 
 
 class MinimalSource(BaseModel):
-    """ Instanciation of a source, store the path and the
-        indexs of the start and the end of the source """
+    """ Represent a source, and store the path and the
+        indexs of the start and the end """
     file_path: str
     first_character_index: int
     last_character_index: int
 
 
 class UnansweredQuestion(BaseModel):
-    """ Instanciation of a question and its ID """
+    """ Represent a question and its ID """
     question_id: str = Field(default_factory=lambda: str(uuid.uuid4()))
     question: str
 
 
 class AnsweredQuestion(BaseModel):
-    """ Store the answer and the sources """
+    """ Represent the answer and the sources """
     sources: list[MinimalSource]
     answer: str
 
 
 class MinimalSearchResults(BaseModel):
+    """ Represent the question and the search results """
     question_id: str
     question: str
     retrieved_sources: list[MinimalSource]
 
 
 class MinimalAnswer(MinimalSearchResults):
+    """ Represent the answer """
     answer: str
 
 
@@ -39,9 +41,11 @@ class RagDataset(BaseModel):
 
 
 class StudentSearchResults(BaseModel):
+    """ Represent the search results """
     search_results: List[MinimalSearchResults]
     k: int
 
 
 class StudentSearchResultsAndAnswer(StudentSearchResults):
+    """ Represent the search results and the answer """
     search_results: List[MinimalAnswer]
